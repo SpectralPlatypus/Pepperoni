06fb9f08415abfebcca4c6db953e338e6c2d7e61
SpectralPlatypus <spectralplat@protonmail.com>
2019-05-21 23:31:01 -0400

    Add VoidOut Patch for Deathplane control

---
+++ Pepperoni/Patches/VoidOut.cs
@@ -0,0 +1,16 @@
+﻿using MonoMod;
+
+#pragma warning disable CS0626, CS0414
+namespace Pepperoni.Patches
+{
+    [MonoModPatch("global::VoidOut")]
+    class VoidOut : global::VoidOut
+    {
+        [MonoModIgnore] private bool active = true;
+
+        /// <summary>
+        /// Functionality to toggle deathplanes
+        /// </summary>
+        public void setActive(bool state) => active = state;
+    }
+}
